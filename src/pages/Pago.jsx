7712814b019dddf83054b26e21280feb754e7e0e
Sara <sbenelramirez@gmail.com>
2020-07-07 23:56:20 -0500

    reestructuracion de componente NuevoPago

--- src/pages/Pago.jsx
+++ src/pages/Pago.jsx
@@ -9,19 +9,11 @@ export default function Pago() {
 	return (
 		<div className='col-11 row mt-3 mb-3'>
 			<div className='col-lg-6 col-xl-6'>
-				<div className='card mb-3' style={{ width: '100%' }}>
-					<div className='card-body'>
-						<h5 className='card-title'>Datos del pago</h5>
-						<NuevoPago />
-					</div>
-				</div>
+				<NuevoPago />
 				<ListadoPedidos />
-				
 			</div>
-
 			<div className='col-lg-6 col-xl-6'>
 				<ListadoPagos />
-				
 			</div>
 		</div>
 	);
