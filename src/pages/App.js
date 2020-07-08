7f9a56944fa9602d3a2b396aacaf2b4eb6e2b8dc
jcarlos5 <josecarlosvilchezvillegas@gmail.com>
2020-07-07 22:17:03 -0500

    avance login

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import VerticalMenu from '../components/Menu/VerticalMenu';
 import { Switch, Route } from 'react-router-dom';
+import {UserContextProvider} from '../context/UserContext';
+
 import Reclamo from './Reclamo';
 import Mesa from './Mesa';
 import Producto from './Producto';
@@ -11,20 +13,22 @@ import Login from './Login';
 
 function App() {
 	return (
-		<div className='container-fluid'>
-			<div className='row'>
-				<VerticalMenu />
-				<Switch>
-					<Route exact path='/' component={Main} />
-					<Route exact path='/login' component={Login} />
-					<Route exact path='/venta' component={Venta} />
-					<Route exact path='/reclamo' component={Reclamo} />
-					<Route exact path='/mesa' component={Mesa} />
-					<Route exact path='/producto' component={Producto} />
-					<Route exact path='/pago' component={Pago} />
-				</Switch>
+		<UserContextProvider>
+			<div className='container-fluid'>
+				<div className='row'>
+					<VerticalMenu />
+					<Switch>
+						<Route exact path='/' component={Main} />
+						<Route exact path='/login' component={Login} />
+						<Route exact path='/venta' component={Venta} />
+						<Route exact path='/reclamo' component={Reclamo} />
+						<Route exact path='/mesa' component={Mesa} />
+						<Route exact path='/producto' component={Producto} />
+						<Route exact path='/pago' component={Pago} />
+					</Switch>
+				</div>
 			</div>
-		</div>
+		</UserContextProvider>
 	);
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import useUser from '../hooks/useUser'
 
 export default function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const Location = useHistory()
+    const {login, isLogged} = useUser()
+
+    useEffect( () => {
+        if(isLogged) Location.push('/')
+    }, [isLogged])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        login(usuario, clave)
+        //alert(`${username}, ${password}`)
+        //Location.push('/')
+    }
 	return (
 		<div className='row'>
-            <div className='col-lg-2 col-xl-2' style={{ backgroundColor:'#E4E4E4' }}>
+            <div className='col-lg-6 col-xl-6' style={{ backgroundColor:'#E4E4E4' }}>
                 <img src={process.env.PUBLIC_URL + '/logo.png'} alt="logo" className='img-fluid'/>
             </div>
     
-            <div className='col-lg-8 col-xl-8'>
-                <div className="row justify-content-center">
+            <div className='col-lg-6 col-xl-6'>
+                <div className="row justify-content-end">
                     <div className="col-8 text-center" >
                         <h1>Iniciar Sesión</h1>
-                        <div className="input-group flex-nowrap mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                    <i className='fa fa-user'></i>
-                                </span>
+                        <form onSubmit={handleSubmit}>
+                            <div className="input-group flex-nowrap mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text">
+                                        <i className='fa fa-user'></i>
+                                    </span>
+                                </div>
+                                <input type="text" className="form-control" placeholder="Username" onChange={ e => setUsername(e.target.value) } value={username} />
                             </div>
-                            <input type="text" className="form-control" placeholder="Username" />
-                        </div>
-                        <div className="input-group flex-nowrap mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                    <i className='fa fa-key'></i>
-                                </span>
+                            <div className="input-group flex-nowrap mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text">
+                                        <i className='fa fa-key'></i>
+                                    </span>
+                                </div>
+                                <input type="password" className="form-control" placeholder="Password" onChange={ e => setPassword(e.target.value) } value={password} />
                             </div>
-                            <input type="text" className="form-control" placeholder="Password" />
-                        </div>
-                        <button className='btn btn-shift'>Iniciar Sesión</button>
+                            <button className='btn btn-shift'>Iniciar Sesión</button>
+                        </form>
                     </div>
                 </div>
             </div>                
